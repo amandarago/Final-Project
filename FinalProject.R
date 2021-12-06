599de32044c01177479bf9bcd40f7d1e43846569
amandarago <84872956+amandarago@users.noreply.github.com>
2021-12-05 19:31:28 -0800

    Add files via upload

---
+++ FinalProject.R
@@ -0,0 +1,5 @@
+# predict hourly bike sharing demand in each of the regions and as a whole 
+# i.e, predict within each hour how many trips begin (start times there are)
+# do ARIMA for each region 
+library(rio)
+?rbind
